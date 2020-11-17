af00fefcac214be0a9b47132c2b1538c18dd2a14
shibafu <shibafu528@gmail.com>
2020-11-17 09:33:24 +0900

    stub

--- dtvault-central/src/main.rs
+++ dtvault-central/src/main.rs
@@ -1,12 +1,40 @@
 use tonic::{transport::Server, Request, Response, Status};
 
+use dtvault_types::shibafu528::dtvault::central::create_program_response::Status as ResponseStatus;
 use dtvault_types::shibafu528::dtvault::central::program_service_server::{
     ProgramService as ProgramServiceTrait, ProgramServiceServer,
 };
 use dtvault_types::shibafu528::dtvault::central::{
     CreateProgramRequest, CreateProgramResponse, UpdateProgramMetadataRequest, UpdateProgramMetadataResponse,
 };
-use dtvault_types::shibafu528::dtvault::ProgramIdentity;
+use dtvault_types::shibafu528::dtvault::{Channel, Program, ProgramIdentity, Service};
+use once_cell::sync::Lazy;
+use std::collections::HashMap;
+use std::sync::Mutex;
+
+fn validate_channel(value: &Channel) -> Result<&Channel, String> {
+    if value.channel.is_empty() {
+        return Err("Invalid value: channel".to_string());
+    }
+    if value.name.is_empty() {
+        return Err("Invalid value: name".to_string());
+    }
+
+    Ok(value)
+}
+
+fn validate_service(value: &Service) -> Result<&Service, String> {
+    if value.service_id == 0 {
+        return Err("Invalid value: service_id".to_string());
+    }
+    if let Some(channel) = &value.channel {
+        if let Err(msg) = validate_channel(channel) {
+            return Err(format!("Violation in channel => {}", msg));
+        }
+    }
+
+    Ok(value)
+}
 
 fn validate_program_id(value: &ProgramIdentity) -> Result<&ProgramIdentity, String> {
     if value.service_id == 0 {
@@ -22,6 +50,9 @@ fn validate_program_id(value: &ProgramIdentity) -> Result<&ProgramIdentity, Stri
     Ok(value)
 }
 
+// TODO: ちゃんと保存する
+static PROGRAM_STORE: Lazy<Mutex<HashMap<String, Program>>> = Lazy::new(|| Mutex::new(HashMap::new()));
+
 #[derive(Debug, Default)]
 struct ProgramService;
 
@@ -31,16 +62,61 @@ impl ProgramServiceTrait for ProgramService {
         &self,
         request: Request<CreateProgramRequest>,
     ) -> Result<Response<CreateProgramResponse>, Status> {
-        Err(Status::unimplemented("not implemented yet"))
+        let msg = request.into_inner();
+        let program = match msg.program {
+            Some(p) => Ok(p),
+            None => Err(Status::failed_precondition("Missing value: program".to_string())),
+        }?;
+
+        if program.service_id == 0 {
+            return Err(Status::failed_precondition("Invalid value: service_id".to_string()));
+        }
+        if program.event_id == 0 {
+            return Err(Status::failed_precondition("Invalid value: event_id".to_string()));
+        }
+        if program.start_at == None {
+            return Err(Status::failed_precondition("Missing value: start_at".to_string()));
+        }
+        if program.name.is_empty() {
+            return Err(Status::failed_precondition("Invalid value: name".to_string()));
+        }
+        if let Some(service) = &program.service {
+            if let Err(msg) = validate_service(service) {
+                return Err(Status::failed_precondition(format!("Violation in service => {}", msg)));
+            }
+        }
+
+        println!("Accept => {:#?}", program);
+        let (status, ret_program) = match PROGRAM_STORE.lock() {
+            Ok(mut store) => {
+                let start_at = program.start_at.as_ref().unwrap();
+                let id = format!(
+                    "{}_{}_{}_{}_{}",
+                    program.network_id, program.service_id, program.event_id, start_at.seconds, start_at.nanos
+                );
+                if let Some(prog) = store.get(&id) {
+                    Ok((ResponseStatus::AlreadyExists, prog.clone()))
+                } else {
+                    store.insert(id, program.clone());
+                    Ok((ResponseStatus::Created, program))
+                }
+            }
+            Err(e) => Err(Status::aborted(format!("{}", e))),
+        }?;
+
+        let res = CreateProgramResponse {
+            status: status.into(),
+            program: Some(ret_program),
+        };
+        Ok(Response::new(res))
     }
 
     async fn update_program_metadata(
         &self,
         request: Request<UpdateProgramMetadataRequest>,
     ) -> Result<Response<UpdateProgramMetadataResponse>, Status> {
-        println!("{:?}", request);
-
         let msg = request.into_inner();
+
         let program_id = match msg.program_id {
             Some(program_id) => match validate_program_id(&program_id) {
                 Ok(_) => Ok(program_id),
@@ -51,7 +127,6 @@ impl ProgramServiceTrait for ProgramService {
             },
             None => Err(Status::failed_precondition("Missing value: program_id")),
         }?;
-
         if msg.key.is_empty() {
             return Err(Status::failed_precondition("Invalid value: key"));
         }
@@ -76,9 +151,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Server listening on {}", addr);
 
     Server::builder()
-        .add_service(ProgramServiceServer::new(service))
+        .add_service(ProgramServiceServer::with_interceptor(service, request_logger))
         .serve(addr)
         .await?;
 
     Ok(())
+}
+
+fn request_logger(req: Request<()>) -> Result<Request<()>, Status> {
+    println!("Request => {:?}", req);
+    Ok(req)
 }
