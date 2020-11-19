96a303fa5b00ef6bbf71e78f81a3e5a70f721b9e
shibafu <shibafu528@gmail.com>
2020-11-20 00:52:14 +0900

    check program id

--- dtvault-central/src/main.rs
+++ dtvault-central/src/main.rs
@@ -275,7 +275,7 @@ impl VideoStorageServiceTrait for VideoStorageService {
 
             match header {
                 Some(_) => match part {
-                    VideoPart::Datagram(data) => {
+                    VideoPart::Datagram(_data) => {
                         // println!("offset = {}", data.offset);
                     }
                     _ => return Err(Status::invalid_argument("Invalid part: need datagram")),
@@ -283,6 +283,27 @@ impl VideoStorageServiceTrait for VideoStorageService {
                 None => match part {
                     VideoPart::Header(h) => {
                         println!("CreateVideo {:#?}", h);
+
+                        let program_id = match h.program_id.as_ref() {
+                            Some(program_id) => match validate_program_id(&program_id) {
+                                Ok(_) => Ok(program_id),
+                                Err(msg) => {
+                                    Err(Status::invalid_argument(format!("Violation in program_id => {}", msg)))
+                                }
+                            },
+                            None => Err(Status::invalid_argument("Missing value: program_id")),
+                        }?;
+
+                        let id = program_store_key(&program_id);
+
+                        match PROGRAM_STORE.lock() {
+                            Ok(mut store) => match store.get_mut(&id) {
+                                Some(_sp) => {}
+                                None => return Err(Status::not_found(format!("Program not found (id = {})", id))),
+                            },
+                            Err(e) => return Err(Status::aborted(format!("{}", e))),
+                        };
+
                         header = Some(h);
                     }
                     _ => return Err(Status::invalid_argument("Invalid part: need header")),
