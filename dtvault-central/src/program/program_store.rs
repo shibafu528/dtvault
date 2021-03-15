137bf2298fe273bf5c4cc0698e64bf5adb1a7212
shibafu <shibafu528@gmail.com>
2021-03-16 00:42:46 +0900

    ProgramsとVideosを分けてシリアライズする (途中まで)

--- dtvault-central/src/program/program_store.rs
+++ dtvault-central/src/program/program_store.rs
@@ -2,16 +2,17 @@ use crate::program::{Persistence, Program as StoredProgram};
 use crate::program::{ProgramKey, Video as StoredVideo};
 use crate::Config;
 use dtvault_types::shibafu528::dtvault::central::create_program_response::Status as ResponseStatus;
-use dtvault_types::shibafu528::dtvault::central::PersistProgram;
+use dtvault_types::shibafu528::dtvault::central::PersistStore;
 use dtvault_types::shibafu528::dtvault::Program;
 use fs2::FileExt;
-use prost::bytes::Buf;
 use prost::Message;
 use std::collections::BTreeMap;
 use std::io::Write;
 use std::sync::{Arc, RwLock};
+use uuid::Uuid;
 
 type ProgramStoreBackend = BTreeMap<ProgramKey, Arc<StoredProgram>>;
+type VideoStoreBackend = BTreeMap<Uuid, Arc<StoredVideo>>;
 
 #[derive(thiserror::Error, Debug)]
 #[error("poisoned lock: another task failed inside")]
@@ -55,49 +56,63 @@ pub enum InitializeError {
     IoError(#[from] std::io::Error),
     #[error("Protobuf decode error: {0}")]
     DecodeError(#[from] prost::DecodeError),
-    #[error("Protobuf decode error: broken message ({}) in position {}", .description, .position)]
-    BrokenMessage { position: usize, description: String },
+    #[error("Protobuf decode error: broken message ({}) in field {}[{}]", .description, .field_name, .index)]
+    BrokenMessage {
+        field_name: String,
+        index: usize,
+        description: String,
+    },
 }
 
 pub struct ProgramStore {
     config: Arc<Config>,
-    store: RwLock<ProgramStoreBackend>,
+    programs: RwLock<ProgramStoreBackend>,
+    videos: RwLock<VideoStoreBackend>,
 }
 
 impl ProgramStore {
     pub fn new(config: Arc<Config>) -> Result<Self, InitializeError> {
-        let mut store = ProgramStoreBackend::new();
+        let mut programs = ProgramStoreBackend::new();
+        let mut videos = VideoStoreBackend::new();
 
         let path = config.programs_file_path();
         if path.is_file() {
             let bin = std::fs::read(path)?;
-            let mut buf = &bin[..];
-            while buf.has_remaining() {
-                let position = bin.len() - buf.remaining(); // for error report
-                let persisted = PersistProgram::decode_length_delimited(&mut buf)?;
+            let store = PersistStore::decode(&bin[..])?;
+            for (index, persisted) in store.programs.into_iter().enumerate() {
                 let sp = StoredProgram::from_persisted(persisted).map_err(|err| InitializeError::BrokenMessage {
-                    position,
+                    field_name: "programs".to_string(),
+                    index,
                     description: format!("{}", err),
                 })?;
-                store.insert(ProgramKey::from_stored_program(&sp), Arc::new(sp));
+                programs.insert(ProgramKey::from_stored_program(&sp), Arc::new(sp));
+            }
+            for (index, persisted) in store.videos.into_iter().enumerate() {
+                let sv = StoredVideo::from_persisted(persisted).map_err(|err| InitializeError::BrokenMessage {
+                    field_name: "videos".to_string(),
+                    index,
+                    description: format!("{}", err),
+                })?;
+                videos.insert(sv.id, Arc::new(sv));
             }
 
-            println!("{} programs loaded.", store.len());
+            println!("{} programs, {} videos loaded.", programs.len(), videos.len());
         }
 
         Ok(ProgramStore {
             config,
-            store: RwLock::new(store),
+            programs: RwLock::new(programs),
+            videos: RwLock::new(videos),
         })
     }
 
     pub fn all(&self) -> Result<Vec<Arc<StoredProgram>>, MutexPoisonError> {
-        let store = self.store.read().map_err(|_| MutexPoisonError)?;
+        let store = self.programs.read().map_err(|_| MutexPoisonError)?;
         Ok(store.values().cloned().collect())
     }
 
     pub fn find(&self, key: &ProgramKey) -> Result<Option<Arc<StoredProgram>>, MutexPoisonError> {
-        let store = self.store.read().map_err(|_| MutexPoisonError)?;
+        let store = self.programs.read().map_err(|_| MutexPoisonError)?;
         Ok(store.get(key).map(Arc::clone))
     }
 
@@ -105,20 +120,22 @@ impl ProgramStore {
         &self,
         program: Program,
     ) -> Result<(Arc<StoredProgram>, FindOrCreateNotice), MutexPoisonError> {
-        let mut store = self.store.write().map_err(|_| MutexPoisonError)?;
-        let key = ProgramKey::from_program(&program);
-        let mut notice = FindOrCreateNotice::AlreadyExists;
-        let sp = store
-            .entry(key)
-            .or_insert_with(|| {
-                notice = FindOrCreateNotice::Created;
-                Arc::new(StoredProgram::from_exchanged(program.clone()).unwrap())
-            })
-            .clone();
-        if let FindOrCreateNotice::Created = notice {
-            self.persist(&store);
-        }
-        Ok((sp, notice))
+        self.mutation(|skip| {
+            let mut store = self.programs.write().map_err(|_| MutexPoisonError)?;
+            let key = ProgramKey::from_program(&program);
+            let mut notice = FindOrCreateNotice::AlreadyExists;
+            let sp = store
+                .entry(key)
+                .or_insert_with(|| {
+                    notice = FindOrCreateNotice::Created;
+                    Arc::new(StoredProgram::from_exchanged(program.clone()).unwrap())
+                })
+                .clone();
+            if let FindOrCreateNotice::AlreadyExists = notice {
+                *skip = true;
+            }
+            Ok((sp, notice))
+        })
     }
 
     pub fn create_video<'a>(
@@ -126,22 +143,23 @@ impl ProgramStore {
         key: &'a ProgramKey,
         video: StoredVideo,
     ) -> Result<Arc<StoredVideo>, VideoWriteError<'a>> {
-        let mut store = self.store.write().map_err(|_| MutexPoisonError)?;
-        let mut program = match store.get(key) {
-            Some(p) => (**p).clone(),
-            None => return Err(VideoWriteError::ProgramNotFound(key)),
-        };
+        self.mutation(|_| {
+            let mut store = self.programs.write().map_err(|_| MutexPoisonError)?;
+            let mut program = match store.get(key) {
+                Some(p) => (**p).clone(),
+                None => return Err(VideoWriteError::ProgramNotFound(key)),
+            };
 
-        if program.exists_video(&video.provider_id) {
-            return Err(VideoWriteError::AlreadyExists(video.provider_id.clone()));
-        }
+            if program.exists_video(&video.provider_id) {
+                return Err(VideoWriteError::AlreadyExists(video.provider_id.clone()));
+            }
 
-        let video = Arc::new(video);
-        program.videos_mut().push(video.clone());
-        store.insert(key.clone(), Arc::new(program));
-        self.persist(&store);
+            let video = Arc::new(video);
+            program.videos_mut().push(video.clone());
+            store.insert(key.clone(), Arc::new(program));
 
-        Ok(video)
+            Ok(video)
+        })
     }
 
     pub fn update_program_metadata<'a>(
@@ -150,34 +168,50 @@ impl ProgramStore {
         metadata_key: &str,
         metadata_value: &str,
     ) -> Result<(), MetadataWriteError<'a>> {
-        let mut store = self.store.write().map_err(|_| MutexPoisonError)?;
-        match store.get(key) {
-            Some(sp) => {
-                let mut sp = (**sp).clone();
-                sp.metadata_mut()
-                    .insert(metadata_key.to_string(), metadata_value.to_string());
-                store.insert(key.clone(), Arc::new(sp));
-                self.persist(&store);
-                Ok(())
+        self.mutation(|_| {
+            let mut store = self.programs.write().map_err(|_| MutexPoisonError)?;
+            match store.get(key) {
+                Some(sp) => {
+                    let mut sp = (**sp).clone();
+                    sp.metadata_mut()
+                        .insert(metadata_key.to_string(), metadata_value.to_string());
+                    store.insert(key.clone(), Arc::new(sp));
+                    Ok(())
+                }
+                None => Err(MetadataWriteError::ProgramNotFound(key)),
             }
-            None => Err(MetadataWriteError::ProgramNotFound(key)),
+        })
+    }
+
+    fn mutation<F: FnOnce(&mut bool) -> Result<T, U>, T, U: From<MutexPoisonError>>(&self, op: F) -> Result<T, U> {
+        let mut skip = false;
+        let result = op(&mut skip)?;
+        if !skip {
+            self.persist()?;
         }
+        Ok(result)
     }
 
     // TODO: 非同期化する
-    fn persist(&self, store: &ProgramStoreBackend) {
+    fn persist(&self) -> Result<(), MutexPoisonError> {
+        let programs = self.programs.read().map_err(|_| MutexPoisonError)?;
+        let videos = self.videos.read().map_err(|_| MutexPoisonError)?;
+
         let path = self.config.programs_file_path();
         let file = std::fs::File::create(path).unwrap();
         file.lock_exclusive().unwrap();
 
         let mut writer = std::io::BufWriter::new(&file);
-        for program in store.values() {
-            let mut buf: Vec<u8> = vec![];
-            program.persist().encode_length_delimited(&mut buf).unwrap();
-            writer.write(&buf).unwrap();
-        }
+        let persisted = PersistStore {
+            programs: programs.values().map(|p| p.persist()).collect(),
+            videos: videos.values().map(|v| v.persist()).collect(),
+        };
+        let mut buf: Vec<u8> = vec![];
+        persisted.encode(&mut buf).unwrap();
+        writer.write(&buf).unwrap();
         writer.flush().unwrap();
 
         file.unlock().unwrap();
+        Ok(())
     }
 }
