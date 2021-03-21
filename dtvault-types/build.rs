c1fc14da28a8afb48e75ef33f12d796d2c109f05
shibafu <shibafu528@gmail.com>
2021-03-21 15:39:35 +0900

    cargo:rerun-if-changedを使う

--- dtvault-types/build.rs
+++ dtvault-types/build.rs
@@ -1,4 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-changed=../proto");
     tonic_build::configure().compile(
         &[
             "../proto/program_service.proto",
